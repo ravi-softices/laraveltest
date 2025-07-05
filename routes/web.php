676bb9762a7b1da41baffc2d7dbff3cf8c0a12e2
ravi-softices <ravi.gangadiya@softices.com>
2025-07-05 11:36:07 +0530

    a

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    $ip = $request->ip();
+    var_dump($ip);
+        $userAgent = $request->userAgent();
+        var_dump($userAgent);
+    // return view('welcome');
 });
+
